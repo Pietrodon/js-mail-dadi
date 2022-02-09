66fa07fe089781622ae35460322fea4c53beae91
Pietrodon <donvitopietro00@gmail.com>
2022-02-09 16:09:05 +0100

    errore

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+// richiamo elemnti html
+let htmlElementUser = document.getElementById('numero-user')
+let htmlElementComputer = document.getElementById('numero-computer')
+
+
 // creazione numero random per l'utente
 let numbUser = Math.round(Math.random () * 5 + 1)
+htmlElementUser.innerHTML += numbUser 
 console.log(numbUser)
 
 // creazione numero random per il computer
 let numbComputer = Math.round(Math.random () * 5 + 1)
+htmlElementComputer.innerHTML += numbComputer
 console.log(numbComputer)
+
+// richiamo elemento per il risultoto 
+let htmlElementRisultato = document.getElementById('risultato')
 
 // confronto numeri per capire chi ha vinto 
 if(numbUser > numbComputer){
-    alert('Complimenti hai vinto')
+    let esito = ('Complimenti hai vinto') 
 } else if (numbUser < numbComputer){
-    alert('Peccato hai perso')
+    let esito = ('Peccato hai perso')
 } else {
-    alert('Pareggio')
+    let esito = ('Pareggio')
 } 
-    
 
+    
+htmlElementRisultato.innerHTML += esito
 
 
