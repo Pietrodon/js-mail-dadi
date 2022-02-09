c87f59300d807b9d63d981af8eba67cb719bfcbd
Pietrodon <donvitopietro00@gmail.com>
2022-02-09 15:34:35 +0100

    calcolo numero random per utente e computer e confronto per il risultato

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert('ciao')
+// creazione numero random per l'utente
+let numbUser = Math.round(Math.random () * 5 + 1)
+console.log(numbUser)
+
+// creazione numero random per il computer
+let numbComputer = Math.round(Math.random () * 5 + 1)
+console.log(numbComputer)
+
+// confronto numeri per capire chi ha vinto 
+if(numbUser > numbComputer){
+    alert('Complimenti hai vinto')
+} else if (numbUser < numbComputer){
+    alert('Peccato hai perso')
+} else {
+    alert('Pareggio')
+} 
+    
+
+
+
